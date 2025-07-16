fe57b92603e69db27cd8c85594b24a791aa04bfc
Olaf Ratajczak <olafrbiznes@gmail.com>
2025-07-16 14:35:45 +0200

    Changed idea

---
+++ Java/Easy/MergeTwoSortedLists_021.java
@@ -0,0 +1,27 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class MergeTwoSortedLists_021 {
+
+    public static void main(String[] args) {
+        ListNode ld = new ListNode(2);
+    }
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+}
+
+
